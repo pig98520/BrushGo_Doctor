6dfb1cea6a14084310ca2c54aff16c55e83ae84f
Swlabgroup2 <swlabgroup2@gmail.com>
2017-07-21 17:31:43 +0800

    2017/07/21 New Project

---
+++ app/src/main/java/com/example/pig98520/brushgo_doctor/tooth_activity.java
@@ -0,0 +1,17 @@
+package com.example.pig98520.brushgo_doctor;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pig98520 on 2017/7/21.
+ */
+
+public class tooth_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tooth_activity);
+    }
+}
