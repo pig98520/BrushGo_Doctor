2cdc469b7d61f489e246a46b57e30c94e802c307
Swlabgroup2 <swlabgroup2@gmail.com>
2017-07-22 16:19:28 +0800

    2017/07/22 spinner建立

---
+++ app/src/main/java/com/example/pig98520/brushgo_doctor/DB_profile.java
@@ -0,0 +1,17 @@
+package com.example.pig98520.brushgo_doctor;
+
+/**
+ * Created by pig98520 on 2017/7/22.
+ */
+
+public class DB_profile {
+    private String name;
+    public DB_profile() {
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/com/example/pig98520/brushgo_doctor/main_activity.java
+++ app/src/main/java/com/example/pig98520/brushgo_doctor/main_activity.java
@@ -2,8 +2,23 @@ package com.example.pig98520.brushgo_doctor;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class main_activity extends AppCompatActivity {
+    private Spinner name_spin;
+    private List<String> nameList = new ArrayList<String>();
+    private ArrayAdapter<String> nameAdapter;
+    private DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +29,30 @@ public class main_activity extends AppCompatActivity {
     }
 
     private void processView() {
-
+        name_spin= (Spinner) findViewById(R.id.name_spin);
     }
 
-
     private void processControl() {
+        nameAdapter = new ArrayAdapter<String>(this, R.layout.spinner_style, nameList);
+        nameAdapter.setDropDownViewResource(R.layout.spinner_style);
+        name_spin.setAdapter(nameAdapter);
 
+        dbRef = FirebaseDatabase.getInstance().getReference().child("profile");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+                    DB_profile data = snapshot.getValue(DB_profile.class);
+                    nameList.add(data.getName());
+                }
+                nameAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
