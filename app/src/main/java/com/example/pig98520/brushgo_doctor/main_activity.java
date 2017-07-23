d4eff5c54d18ecffbf302e0a30bf3ad90774da2b
Swlabgroup2 <swlabgroup2@gmail.com>
2017-07-23 16:00:30 +0800

    2017/07/22 正常讀取牙齒狀況及回診日期

--- app/src/main/java/com/example/pig98520/brushgo_doctor/main_activity.java
+++ app/src/main/java/com/example/pig98520/brushgo_doctor/main_activity.java
@@ -8,6 +8,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,13 +17,17 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class main_activity extends AppCompatActivity {
     private Spinner name_spin;
     private String name;
     private String uid;
+    private String nowDate;
+    private TextView install_date;
     private Button confirm;
     private List<String> nameList = new ArrayList<String>();
     private ArrayAdapter<String> nameAdapter;
@@ -44,6 +49,7 @@ public class main_activity extends AppCompatActivity {
         nameAdapter.setDropDownViewResource(R.layout.spinner_style);
         name_spin.setAdapter(nameAdapter);
         confirm=(Button)findViewById(R.id.confirm_btn);
+        install_date=(TextView)findViewById(R.id.txt_installDate);
         intent = new Intent();
         bundle= new Bundle();
     }
@@ -70,6 +76,21 @@ public class main_activity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 name=name_spin.getSelectedItem().toString();
+                Query query = dbRef.orderByChild("name").equalTo(name);
+                query.addValueEventListener(new ValueEventListener() {
+                    public void onDataChange(DataSnapshot snapshot) {
+                        for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                            uid=dataSnapshot.getKey().toString();
+/*                            Toast.makeText(main_activity.this,uid,Toast.LENGTH_LONG).show();*/
+                            checkDate();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
             }
 
             @Override
@@ -81,25 +102,54 @@ public class main_activity extends AppCompatActivity {
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Query query = dbRef.orderByChild("name").equalTo(name);
-                query.addValueEventListener(new ValueEventListener() {
-                    public void onDataChange(DataSnapshot snapshot) {
-                        for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
-                            uid=dataSnapshot.getKey().toString();
-                            bundle.putString("uid", uid);
-                            intent.setClass(main_activity.this, tooth_activity.class);
-                            intent.putExtras(bundle);
-                            startActivity(intent);
-/*                            Toast.makeText(main_activity.this,uid,Toast.LENGTH_LONG).show();*/
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
+/*                checkupDialog();*/
+                bundle.putString("uid", uid);
+                intent.setClass(main_activity.this, tooth_activity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
             }
         });
     }
+
+    private void checkDate() {
+        dbRef.child(uid).child("install_date").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                nowDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+                install_date.setText("上次看診日期:"+dataSnapshot.getValue().toString().trim());
+
+                if(dataSnapshot.getValue().toString().trim().equals(nowDate))
+                    bundle.putBoolean("back",false);
+                else
+                    bundle.putBoolean("back",true);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    /*private void checkupDialog() {
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("病患狀況");
+        dialog.setMessage("請問是否為BrushGo回診病患?");
+        DialogInterface.OnClickListener yesClick =new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                bundle.putBoolean("back",true);
+            }
+        };
+        DialogInterface.OnClickListener noClick=new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                bundle.putBoolean("back",false);
+            }
+        };
+
+        dialog.setNeutralButton("是",yesClick);
+        dialog.setNegativeButton("否",noClick);
+        dialog.show();
+    }*/
 }
--- app/src/main/java/com/example/pig98520/brushgo_doctor/tooth_activity.java
+++ app/src/main/java/com/example/pig98520/brushgo_doctor/tooth_activity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by pig98520 on 2017/7/21.
@@ -16,9 +18,10 @@ import com.google.firebase.database.FirebaseDatabase;
 public class tooth_activity extends AppCompatActivity {
     private Bundle bundle;
     private String uid;
+    private boolean back;
     private DatabaseReference dbRef;
-    private int i=0;
     private Button tooth[]=new Button[28];
+    private Boolean status[]=new Boolean[28];
     private int id[]=new int[]
             {R.id.tooth_1,
             R.id.tooth_2,
@@ -54,28 +57,74 @@ public class tooth_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tooth_activity);
         processView();
+        setToothCondition();
         processControl();
+    }
+
+    private void setToothCondition() {
+        if(!back)
+        {
+            for(int i=0;i<tooth.length;i++)
+                dbRef.child("tooth").child(uid).child(i+1+"").setValue("g");
+        }
+        else{
+            for(int j=0;j<tooth.length;j++) {
+                final int finalJ = j;
+                dbRef.child("tooth").child(uid).child(j+1+"").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.getValue().toString().trim().equals("b"))
+                        {
+                            tooth[finalJ].setBackgroundResource(R.drawable.tooth_dirty_24);
+                            status[finalJ]=false;
+                        }
+                        else
+                        {
+                            tooth[finalJ].setBackgroundResource(R.drawable.tooth_clean_24);
+                            status[finalJ]=true;
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        }
     }
 
 
     private void processView() {
-        for(i=0;i<tooth.length;i++)
-            tooth[i]=(Button)findViewById(id[i]);
+        for(int i=0;i<tooth.length;i++) {
+            tooth[i] = (Button) findViewById(id[i]);
+        }
         dbRef= FirebaseDatabase.getInstance().getReference();
         bundle = this.getIntent().getExtras();
         uid = bundle.getString("uid");
-/*        Toast.makeText(tooth_activity.this,uid,Toast.LENGTH_LONG).show();*/
+        back=bundle.getBoolean("back");
     }
 
     private void processControl() {
-        for(i=0;i<tooth.length;i++)
+        for(int i=0;i<tooth.length;i++)
         {
-            tooth[i].setOnClickListener(new View.OnClickListener() {
+            final int finalI = i;
+            tooth[finalI].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(tooth_activity.this,"Test",Toast.LENGTH_LONG).show();
+                    if(!status[finalI]){
+                        tooth[finalI].setBackgroundResource(R.drawable.tooth_clean_24);
+                        status[finalI]=true;
+                        dbRef.child("tooth").child(uid).child(finalI+1+"").setValue("g");
+                    }
+                    else {
+                        tooth[finalI].setBackgroundResource(R.drawable.tooth_dirty_24);
+                        status[finalI]=false;
+                        dbRef.child("tooth").child(uid).child(finalI+1+"").setValue("b");
+                    }
                 }
             });
         }
     }
+
 }
